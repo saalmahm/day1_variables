007f8ed36d6701d04f38b8cf89ab4d901d73b9cf
salma hamdi <saalmahm@gmail.com>
2024-09-24 11:39:45 +0100

    challenge 3

---
+++ challeng3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+float km,Yards;
+  printf("Entrer la distance en kilomètres:\n");
+  scanf("%f",&km);
+  Yards=km * 1093.61;
+  printf("la distance en yards est:\n %f ",Yards);
+  return 0;
+}
