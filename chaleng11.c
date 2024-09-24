6ab2ad3080aad0f5a9252cc89aee120847d68f05
salma hamdi <saalmahm@gmail.com>
2024-09-24 15:47:08 +0100

    challenge 11

---
+++ chaleng11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+int l,L,s;
+const float pi=3.14;
+  printf("Entrer la longueur :\n");
+  scanf("%d",&L);
+  printf("Entrer la Largeur :\n");
+  scanf("%d",&l);
+  s=L*l;
+  printf("la surface est:%d",s);
+  return 0;
+}
