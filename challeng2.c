9ab76c2f0947d3707b50594fb80328ba0dec30bb
salma hamdi <saalmahm@gmail.com>
2024-09-24 11:23:29 +0100

    challenge 2

---
+++ challeng2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+float C,k;
+  printf("Entrer la température en Celsius:\n");
+  scanf("%f",&C);
+  k=C+273.15;
+  printf("a température en Kelvin est:\n %f ",C);
+  return 0;
+}
